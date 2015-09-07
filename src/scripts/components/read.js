7a963a0dd9ed3a29eb795227635c647c44f1807e
Lucas Reis <lucasmreis@gmail.com>
2015-09-06 21:19:29 -0300

    initial read screen

--- src/scripts/components/read.js
+++ src/scripts/components/read.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import Mori from 'mori';
+
+let {get, nth} = Mori;
 
 const str = m => JSON.stringify(Mori.toJs(m), null, '  ');
 
 export default class Read extends React.Component {
+  current() {
+    const state = this.props.appState;
+    const currentIndex = get(state, 'currentSentence');
+    const sentences = get(state, 'sentences');
+    const currentSentence = nth(sentences, currentIndex);
+    return get(currentSentence, 'text');
+  }
+
   render() {
     window.location.hash = '/read';
-    return <div>
-      <h1>Read</h1>
-      <pre>{str(this.props.appState)}</pre>
+    return <div className="container read-container">
+      <div className="happy-sentence">{this.current()}</div>
     </div>;
   }
 }
