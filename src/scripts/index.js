c5d373e7cbc177a82694b4af9a43090f3cf0e8c3
Lucas Reis <lucasmreis@gmail.com>
2015-09-09 01:26:12 -0300

    render as a process

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,17 @@
 import Polyfill from 'babel/polyfill';
 import Mori from 'mori';
-import {chan, go, take, offer, put} from 'js-csp';
+import {chan, go, take, offer, put, putAsync, buffers} from 'js-csp';
+import React from 'react';
+React.initializeTouchEvents(true);
 
 import * as Data from './data';
-import {requestRender} from './render';
+import Main from './components/main';
 
 let {toClj, get} = Mori;
 
 const loadApp = () => ({
   state: Data.fresh(),
+  renderCh: chan(buffers.sliding(1)),
   channels: {
     nav: chan(),
     view: chan(),
@@ -51,21 +54,41 @@ const initUpdates = app => {
         const value = yield take(ch);
         console.log(`On channel [ ${k} ] received value [ ${JSON.stringify(value)} ]`);
         app.state = updateFn(app.state, value);
-        requestRender(app);
+        yield put(app.renderCh, app.state);
       }
     });
   });
 }
+
+const initRender = (app, element) => {
+  putAsync(app.renderCh, app.state);
+
+  go(function* () {
+    while(true) {
+      const state = yield take(app.renderCh);
+      const finishRender = chan();
+      window.finishRender = finishRender;
+      React.render(
+      //window.requestAnimationFrame(() => React.render(
+        <Main appState={app.state} channels={app.channels} />,
+        element,
+        () => window.requestAnimationFrame(() => putAsync(finishRender, {})));
+      yield take(finishRender);
+    }
+  });
+};
 
 const start = () => {
   let app = loadApp();
   window.app = app; // for testing
   initHistory(app);
   initUpdates(app);
-  requestRender(app);
+  initRender(app, document.getElementById('happyme'));
 };
 
 start();
 
+window.start = start;
 window.offer = offer;
 window.Mori = Mori;
+window.csp = require('js-csp');
